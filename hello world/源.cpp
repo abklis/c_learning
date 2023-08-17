8a0ad88c3c29f8a0050d242ff4e6b7587975bd1b
家豪 杨 <18382820135@163.com>
2023-08-17 20:51:23 +0800

    8_17

--- hello world/源.cpp
+++ hello world/源.cpp
@@ -1,7 +1,19 @@
 #include<stdio.h>
+//int main()
+//{
+//	printf("Hello world\n");
+//	printf("%d", 100);//打印整数
+//	return 0;
+//}
 int main()
-{
-	printf("Hello world\n");
-	printf("%d", 100);//打印整数
+{//创建一个变量
+	//类型 变量名字 = 0；
+	int age = 18;
+	age = age + 1;
+	printf("%d\n", age);
+	float weight = 66.5;
+	printf("%f", weight);
 	return 0;
 }
+//%d-整形
+//%f-float    %lf-double
